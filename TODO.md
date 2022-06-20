f9b60ce6041fd0fc791b70bf3c24c846bbe5857a
solomon234 <solomonmuratov@gmail.com>
2022-06-20 09:53:25 -0400

    Created todoand commented the main file

---
+++ TODO.md
@@ -0,0 +1,12 @@
+### Operators
+
+- [ ] Addition
+- [ ] Subtraction
+- [ ] Divison
+- [ ] Multiplication
+
+### Stories
+- [ ] As a user i would like options presented for me to identify the operation i want
+- [ ] As a user i would like to input two numbers 
+- [ ] As a user i would like the two numbers and the result shown to me
+- [ ] As a user i would like the optioon to use the calculator again
---
+++ project/main.py
@@ -0,0 +1,20 @@
+#Create functions for each operation in TODO.md
+
+#main function should follow the user stores in TODO.md
+def main():
+    valid_choices = [1]
+
+    # Output the options for the end user
+    print("Select operation.")
+
+    while True:
+        # Take input from the user
+
+        # Check if choice is one of the four options
+
+        # Check if user wants another calculation
+        # Break the while loop if answer is no
+        break
+ 
+
+main();
